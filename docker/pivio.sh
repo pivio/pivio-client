92da087007b4e2870e491e33e0d0f9adbe360d94
Ralf Wehner <ralf.wehner@leanix.net>
2017-11-13 11:12:31 +0100

    [#152054030] - improved docker build shell script - added help to pivio.sh wrapper script

--- docker/pivio.sh
+++ docker/pivio.sh
@@ -1,5 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+# ------------------------------------------------------------------
+# Simple wrapper and help script used to start the pivio-client.
+# ------------------------------------------------------------------
+
+
+CONTAINER_NAME="leanix/pivio-client"
+
+
+# --- Option processing --------------------------------------------
+display_usage() { 
+    echo "Shell script used to wrap the pivio client java call within the docker container." 
+    echo -e "\nUsage:\ndocker run -it [docker run options] ${CONTAINER_NAME} pivio [pivio client options]\n" 
+    echo -e "  pivio client options: -help        shows the origin pivio client help (default, if no option is provided)" 
+    echo -e "                        -h, --help   show this help" 
+    echo -e "\nEG:" 
+    echo -e "  docker run --rm -it -v \$(pwd):/source ${CONTAINER_NAME} pivio -serviceurl "https://local-eam.leanix.net/services/integrations/v2/pivio/document" -verbose \n" 
+} 
+
+# check whether user had supplied -h or --help or no parameters . If yes display usage 
+if [[ ( ${@: -1} == "--help") ||  ${@: -1} == "-h" || "$#" == 0 ]] 
+then 
+    display_usage
+    exit 0
+fi 
+
+# --- body ---------------------------------------------------------
 
 cd /source
-
 java -jar /pivio.jar "$@"
