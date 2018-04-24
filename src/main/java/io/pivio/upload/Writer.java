c88d9c79e94ed8b7bbd5a591e955e207b203f01a
Angel Fraga Parodi <angel.fraga.parodi@leanix.net>
2018-04-24 16:23:23 +0200

    [#156914736] Add proxy auth support

--- src/main/java/io/pivio/upload/Writer.java
+++ src/main/java/io/pivio/upload/Writer.java
@@ -1,25 +1,43 @@
 package io.pivio.upload;
 
+import static io.pivio.Configuration.SWITCH_OUTFILE;
+import static io.pivio.Configuration.SWITCH_OUTFILETOPLEVELATTRIBUTES;
+import static io.pivio.Configuration.SWITCH_PROXY_HOSTNAME;
+import static io.pivio.Configuration.SWITCH_PROXY_PORT;
+import static io.pivio.Configuration.SWITCH_PROXY_PWD;
+import static io.pivio.Configuration.SWITCH_PROXY_TYPE;
+import static io.pivio.Configuration.SWITCH_PROXY_USER;
+import static io.pivio.Configuration.SWITCH_SERVICE_URL;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
 import io.pivio.Configuration;
 import io.pivio.Logger;
 import io.pivio.PivioYamlParserException;
 import io.pivio.schema.SchemaValidator;
-import org.codehaus.jackson.schema.JsonSchema;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
-
 import java.io.File;
 import java.io.IOException;
+import java.net.Proxy.Type;
 import java.util.HashMap;
 import java.util.Map;
-
-import static io.pivio.Configuration.*;
+import java.util.OptionalInt;
 
 @Service
 public class Writer {
@@ -60,10 +78,12 @@ public class Writer {
                     String[] attributes = attributeParameter.split(",");
                     for (String attribute : attributes) {
                         if (document.containsKey(attribute)) {
-                            log.verboseOutput("Filtering output to file with attribute '" + attribute + "'.", configuration.isVerbose());
+                            log.verboseOutput("Filtering output to file with attribute '" + attribute + "'.",
+                                configuration.isVerbose());
                             writeToFile.put(attribute, document.get(attribute));
                         } else {
-                            log.verboseOutput("Could not find requested top level attribute '" + attribute + "'.", configuration.isVerbose());
+                            log.verboseOutput("Could not find requested top level attribute '" + attribute + "'.",
+                                configuration.isVerbose());
                         }
                     }
                 } else {
@@ -80,14 +100,17 @@ public class Writer {
 
     private void uploadToServer(String json) {
         if (configuration.hasOption(SWITCH_SERVICE_URL)) {
+
             String serviceUrl = configuration.getParameter(SWITCH_SERVICE_URL);
             log.verboseOutput("Uploading  to " + serviceUrl + ": " + json, configuration.isVerbose());
-            RestTemplate rt = new RestTemplate();
+
+            RestTemplate rt = getRestTemplate();
             rt.setErrorHandler(new RestCallErrorHandler());
             HttpHeaders headers = new HttpHeaders();
             headers.set("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
             try {
-                ResponseEntity<JsonNode> responseEntity = rt.exchange(serviceUrl, HttpMethod.POST, new HttpEntity<>(json, headers), JsonNode.class);
+                ResponseEntity<JsonNode> responseEntity =
+                    rt.exchange(serviceUrl, HttpMethod.POST, new HttpEntity<>(json, headers), JsonNode.class);
                 if (responseEntity.getStatusCode() != HttpStatus.CREATED) {
                     handleNonCreatedStatusCode(serviceUrl, responseEntity, json);
                 } else {
@@ -97,7 +120,8 @@ public class Writer {
                 handleConnectionRefused(serviceUrl);
             }
         } else {
-            log.verboseOutput("Not uploading to any server since no '" + SWITCH_SERVICE_URL + "' parameter was specified.", configuration.isVerbose());
+            log.verboseOutput("Not uploading to any server since no '" + SWITCH_SERVICE_URL + "' parameter was specified.",
+                configuration.isVerbose());
         }
     }
 
@@ -111,11 +135,87 @@ public class Writer {
     }
 
     private void handleNonCreatedStatusCode(String serverUrl, ResponseEntity<JsonNode> responseEntity, String payload) {
-        String message = "Error: Upload to " + serverUrl + " with payload '" + payload + "' failed.\nReturn code: " + responseEntity.getStatusCode() + " with Message " + responseEntity.getBody().toString() + ".";
+        String message = "Error: Upload to " + serverUrl + " with payload '" + payload + "' failed.\nReturn code: "
+            + responseEntity.getStatusCode() + " with Message " + responseEntity.getBody().toString() + ".";
         if (configuration.hasOption(Configuration.SWITCH_UPLOAD_FAILS_EXIT1)) {
             throw new RuntimeException(message);
         } else {
             log.output(message);
+        }
+    }
+
+    private RestTemplate getRestTemplate() {
+
+        String hostname, username = null, password = null; // password could be null
+        int port = 8080;
+        Type proxyType = Type.HTTP;
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        if (configuration.hasOption(SWITCH_PROXY_HOSTNAME)) {
+            hostname = configuration.getParameter(SWITCH_PROXY_HOSTNAME);
+
+            // getting proxy port
+            if (configuration.hasOption(SWITCH_PROXY_PORT)) {
+                OptionalInt portO = tryParseInt(configuration.getParameter(SWITCH_PROXY_PORT));
+                if (!portO.isPresent()) {
+                    throw new RuntimeException("Proxy port was wrong");
+                } else {
+                    port = portO.getAsInt();
+                }
+            }
+
+            // getting proxy type
+            if (configuration.hasOption(SWITCH_PROXY_TYPE)) {
+                switch (configuration.getParameter(SWITCH_PROXY_TYPE).toUpperCase()) {
+                    case "SOCKET":
+                        proxyType = Type.SOCKS;
+                        break;
+                    case "DIRECT":
+                        proxyType = Type.DIRECT;
+                        break;
+                }
+            }
+
+            HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+            HttpHost proxy = new HttpHost(hostname, port);
+            clientBuilder.setProxy(proxy);
+
+            // check & set proxy credentials
+            if (configuration.hasOption(SWITCH_PROXY_USER)) {
+                username = configuration.getParameter(SWITCH_PROXY_USER);
+                if (!username.isEmpty()) {
+                    if (configuration.hasOption(SWITCH_PROXY_PWD)) {
+                        password = configuration.getParameter(SWITCH_PROXY_PWD);
+                    }
+                    // set credentials
+                    CredentialsProvider credsProvider = new BasicCredentialsProvider();
+                    credsProvider.setCredentials(
+                        new AuthScope(hostname, port),
+                        new UsernamePasswordCredentials(username, password));
+                    clientBuilder.setDefaultCredentialsProvider(credsProvider).disableCookieManagement();
+                }
+            }
+
+            log.output("Using proxy: ON");
+            log.output("\t Url => " + hostname);
+            log.output("\t Port => " + String.valueOf(port));
+            log.output("\t Type => " + proxyType.toString());
+            log.output("\t Username => " + username);
+
+            HttpClient httpClient = clientBuilder.build();
+            factory.setHttpClient(httpClient);
+        }
+
+        RestTemplate rt = new RestTemplate(factory);
+        rt.setErrorHandler(new RestCallErrorHandler());
+        return rt;
+    }
+
+    private OptionalInt tryParseInt(String inputValue) {
+        try {
+            return OptionalInt.of(Integer.parseInt(inputValue));
+        } catch (NumberFormatException e) {
+            return OptionalInt.empty();
         }
     }
 }
